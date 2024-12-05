c1039d14a492ed88b906cb66d44b1f3bcbcb767e
nohannah <nohannah508@gmail.com>
2024-12-05 22:11:02 +0600

    movie

--- Function/foreach.js
+++ Function/foreach.js
@@ -1,6 +1,28 @@
-const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-num.forEach(function(number) {
-    if (number % 2 === 0) {
-        console.log(number);
+// const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// num.forEach(function(number) {
+//     if (number % 2 === 0) {
+//         console.log(number);
+//     }
+// });
+const movies =[
+    {
+        title:'Avatar',
+        score:99
+    },
+    {
+        title:'Moana',
+        score:90
+    },
+    {
+        title:'Santa',
+        score:70
+    },
+    {
+        title:'kungFupanda',
+        score:70
     }
-});
+]
+movies.forEach (function(movie)
+{
+    console.log(`${movie.title} - ${movie.score/100}`)
+})
---
+++ Function/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+
+        score:70
+    }
+]
+movies.forEach (function(movie)
