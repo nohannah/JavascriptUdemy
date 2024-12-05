cc343187c9d30610f0ee73227892c172b2f8edfc
nohannah <nohannah508@gmail.com>
2024-12-05 22:29:59 +0600

    map

--- Function/Map.js
+++ Function/Map.js
@@ -1,6 +1,31 @@
-const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-const double =num.map(function(num) {
-    return num * 2; 
+// // mapp
+// const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// const double =num.map(function(num) {
+//     return num * 2; 
     
-})
-console.log(double)
+// })
+// console.log(double)
+const movies =[
+    {
+        title:'Avatar',
+        score:99
+    },
+    {
+        title:'Moana',
+        score:90
+    },
+    {
+        title:'Santa',
+        score:70
+    },
+    {
+        title:'kungFupanda',
+        score:70
+    }
+]
+const titles = movies.map(function(movie)
+{
+    return movie.title.toUpperCase();
+}
+)
+console.log(titles)
